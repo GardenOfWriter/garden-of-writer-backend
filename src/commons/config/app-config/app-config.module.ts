05181a46749284e104617e5cb4566c35adeccf0f
LeeSeokHo <rrgks@naver.com>
2023-06-02 18:44:27 +0900

    [#3]/feat: ConfigModule validate

--- src/commons/config/app-config/app-config.module.ts
+++ src/commons/config/app-config/app-config.module.ts
@@ -1,10 +1,32 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppConfigService } from './app-config.service';
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string().required(),
+
+        DATABASE_TYPE: Joi.string().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_DATABASE: Joi.string().required(),
+
+        JWT_ACCESS_KEY: Joi.string().required(),
+        JWT_REFRESH_KEY: Joi.string().required(),
+
+        EMAIL_SENDER: Joi.string().required(),
+        EMAIL_PASS: Joi.string().required(),
+      }),
+    }),
+  ],
   providers: [ConfigService, AppConfigService],
   exports: [ConfigService, AppConfigService],
 })
