c60b7ec3fccf1b844558de0fb8dbe1449fa7f056
LeeSeokHo <rrgks@naver.com>
2023-06-02 17:47:51 +0900

    [#3]/feat: ExternalConfigService

--- src/commons/config/app-config.module.ts
+++ src/commons/config/app-config.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppConfigService } from './app-config.service';
+import { ExternalConfigServiceService } from './external-config-service.service';
 
 @Global()
 @Module({
-  imports: [ConfigModule],
-  providers: [ConfigService, AppConfigService],
-  exports: [ConfigService, AppConfigService],
+  imports: [ConfigModule.forRoot()],
+  providers: [ConfigService, AppConfigService, ExternalConfigServiceService],
+  exports: [ConfigService, AppConfigService, ExternalConfigServiceService],
 })
 export class AppConfigModule {}
---
+++ src/commons/config/external-config-service.service.ts
@@ -0,0 +1,42 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CacheModuleOptions, Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
+import { ENV_KEY } from './app-config.constant';
+import { AppConfigService } from './app-config.service';
+
+@Injectable()
+export class ExternalConfigServiceService implements TypeOrmOptionsFactory {
+  constructor(private readonly appConfigService: AppConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: this.appConfigService.get<'mysql'>(ENV_KEY.DATABASE_TYPE),
+      host: this.appConfigService.get<string>(ENV_KEY.DATABASE_HOST),
+      port: this.appConfigService.get<number>(ENV_KEY.DATABASE_PORT),
+      username: this.appConfigService.get<string>(ENV_KEY.DATABASE_USERNAME),
+      password: this.appConfigService.get<string>(ENV_KEY.DATABASE_PASSWORD),
+      database: this.appConfigService.get<string>(ENV_KEY.DATABASE_DATABASE),
+      entities: [__dirname + '/../../' + 'apis/**/*.entity.*'],
+      synchronize: false,
+      logging: true,
+    };
+  }
+
+  createRedisClientOptions(): CacheModuleOptions<RedisClientOptions> {
+    return {
+      store: redisStore,
+      url: 'redis://my-redis:6379',
+      isGlobal: true,
+    };
+  }
+
+  createApolloDriverConfig(): ApolloDriverConfig {
+    return {
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/common/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    };
+  }
+}
