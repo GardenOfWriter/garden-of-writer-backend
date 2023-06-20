36ef571a18f62e5ecf0013074e80f4711ee2f229
LeeSeokHo <rrgks@naver.com>
2023-06-20 14:04:39 +0900

    [#15]/modify: follow count entity

--- src/apis/followCounts/followCount.entity.ts
+++ src/apis/followCounts/followCount.entity.ts
@@ -1,30 +1,52 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { UserEntity } from '@src/apis/user/entities/user.entity';
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity } from '@src/commons/libraries/base-entity';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
 @Entity({ name: 'follow_count' })
 @ObjectType()
-export class FollowCountEntity {
-  @PrimaryGeneratedColumn('uuid')
-  @Field()
-  id: string;
+export class FollowCountEntity extends BaseEntity {
+  @Column({
+    type: 'uuid',
+    name: 'userId',
+    comment: '유저 ID',
+  })
+  @Field(() => String, {
+    description: '유저 ID',
+  })
+  userId: string;
 
-  @Column({ default: 0 })
-  @Field(() => Int)
+  @Column({
+    name: 'follow_count',
+    type: 'int',
+    unsigned: true,
+    default: 0,
+    comment: '팔로우 총 수',
+  })
+  @Field(() => Int, {
+    description: '팔로우 총 수',
+  })
   followCount: number;
 
-  @Column({ default: 0 })
-  @Field(() => Int)
+  @Column({
+    name: 'follower_count',
+    type: 'int',
+    unsigned: true,
+    default: 0,
+    comment: '팔로워 총 수',
+  })
+  @Field(() => Int, {
+    description: '팔로워 총 수',
+  })
   followerCount: number;
 
-  @JoinColumn()
-  @OneToOne(() => UserEntity)
-  @Field(() => UserEntity)
+  @OneToOne(() => UserEntity, (user) => user.id, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'userId', referencedColumnName: 'id' }])
+  @Field(() => UserEntity, {
+    description: '유저',
+  })
   user: UserEntity;
 }
