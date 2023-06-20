3b0408d45f25927a723b05f702927b54d18763e0
LeeSeokHo <rrgks@naver.com>
2023-06-20 14:10:56 +0900

    [#15]/modify: pick entity

--- src/apis/pick/entities/pick.entity.ts
+++ src/apis/pick/entities/pick.entity.ts
@@ -2,36 +2,66 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { BoardEntity } from '@src/apis/board/entities/board.entity';
 import { FictionBoardEntity } from '@src/apis/fiction_board/entities/fiction_board.entity';
 import { UserEntity } from '@src/apis/user/entities/user.entity';
-import {
-  CreateDateColumn,
-  DeleteDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity } from '@src/commons/libraries/base-entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'pick' })
 @ObjectType()
-export class PickEntity {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
-  id: string;
+export class PickEntity extends BaseEntity {
+  @Column({
+    type: 'uuid',
+    name: 'board_id',
+    comment: '유저 ID',
+  })
+  @Field(() => String, {
+    description: '게시글 ID',
+  })
+  boardId: string;
 
-  @ManyToOne(() => BoardEntity, { onDelete: 'CASCADE' })
-  @Field(() => BoardEntity)
+  @Column({
+    type: 'uuid',
+    name: 'fiction_board_id',
+    comment: '유저 ID',
+  })
+  @Field(() => String, {
+    description: '소설 ID',
+  })
+  fictionBoardId: string;
+
+  @Column({
+    type: 'uuid',
+    name: 'user_id',
+    comment: '유저 ID',
+  })
+  @Field(() => String, {
+    description: '유저 ID',
+  })
+  userId: string;
+
+  @ManyToOne(() => BoardEntity, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @Field(() => BoardEntity, {
+    description: '게시글',
+  })
   board: BoardEntity;
 
-  @ManyToOne(() => FictionBoardEntity, { onDelete: 'CASCADE' })
-  @Field(() => FictionBoardEntity)
+  @ManyToOne(() => FictionBoardEntity, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @Field(() => FictionBoardEntity, {
+    description: '소설',
+  })
   fictionBoard: FictionBoardEntity;
 
-  @ManyToOne(() => UserEntity)
-  @Field(() => UserEntity)
+  @ManyToOne(() => UserEntity, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @Field(() => UserEntity, {
+    description: '유저',
+  })
   user: UserEntity;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @DeleteDateColumn()
-  deletedAt: Date;
 }
