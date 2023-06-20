42e4e0ba1fd21d58a82c8683887a219f215841fb
LeeSeokHo <rrgks@naver.com>
2023-06-20 13:40:28 +0900

    [#15]/modify: comment entity

--- src/apis/comment/entities/comment.entity.ts
+++ src/apis/comment/entities/comment.entity.ts
@@ -8,19 +8,29 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 @Entity({ name: 'comment' })
 @ObjectType()
 export class CommentEntity extends BaseEntity {
-  @Column({ type: 'varchar', length: 255 })
+  @Column({
+    type: 'varchar',
+    length: 255,
+    comment: '댓글 본문',
+  })
   @Field(() => String, {
-    description: '댓글',
+    description: '댓글 본문',
   })
   content: string;
 
-  @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
+  @ManyToOne(() => UserEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
   @Field(() => UserEntity, {
     description: '댓글을 작성한 유저',
   })
   user: UserEntity;
 
-  @ManyToOne(() => BoardEntity, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  @ManyToOne(() => BoardEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
   @Field(() => BoardEntity, {
     description: '댓글이 달린 게시글',
   })
