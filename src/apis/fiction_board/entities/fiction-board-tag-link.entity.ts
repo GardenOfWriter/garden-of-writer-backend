ea82858840a926f857985a727f641f0bf50f62d2
LeeSeokHo <rrgks@naver.com>
2023-06-20 13:50:24 +0900

    [#15]/modify: fiction board tag link entity

--- src/apis/fiction_board/entities/fiction-board-tag-link.entity.ts
+++ src/apis/fiction_board/entities/fiction-board-tag-link.entity.ts
@@ -1,47 +1,47 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { FictionBoardEntity } from '@src/apis/fiction_board/entities/fiction_board.entity';
 import { TagEntity } from '@src/apis/tag/entities/tag.entity';
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { BaseEntity } from '@src/commons/libraries/base-entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'fiction_board_tag_link' })
 @ObjectType()
-export class FictionBoardTagLinkEntity {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
-  id: string;
-
-  @Column('uuid', {
+export class FictionBoardTagLinkEntity extends BaseEntity {
+  @Column({
+    type: 'uuid',
     name: 'fiction_board_id',
     comment: '소살 게시글 고유 ID',
   })
+  @Field(() => String, {
+    description: '소설 게시글 고유 ID',
+  })
   fictionBoardId: string;
 
-  @Column('uuid', {
+  @Column({
+    type: 'uuid',
     name: 'tag_id',
     comment: '태그 고유 ID',
   })
+  @Field(() => String, {
+    description: '소설 태그 고유 ID',
+  })
   tagId: string;
 
-  @CreateDateColumn()
-  @Field(() => Date)
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  @Field(() => Date)
-  updatedAt: Date;
-
-  @ManyToOne(() => FictionBoardEntity, { onDelete: 'CASCADE' })
-  @Field(() => FictionBoardEntity)
+  @ManyToOne(() => FictionBoardEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @Field(() => FictionBoardEntity, {
+    description: '소설',
+  })
   fictionBoard: FictionBoardEntity;
 
-  @ManyToOne(() => TagEntity)
-  @Field(() => TagEntity)
+  @ManyToOne(() => TagEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @Field(() => TagEntity, {
+    description: '태그',
+  })
   tag: TagEntity;
 }
