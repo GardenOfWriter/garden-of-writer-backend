0785ccb4fd06053a7ea4bbd81ded39d620585395
LeeSeokHo <rrgks@naver.com>
2023-06-20 13:56:06 +0900

    [#15]/modify: follow entity

--- src/apis/follow/entities/follow.entity.ts
+++ src/apis/follow/entities/follow.entity.ts
@@ -1,31 +1,53 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { UserEntity } from '@src/apis/user/entities/user.entity';
-import {
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { BaseEntity } from '@src/commons/libraries/base-entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
+/**
+ * @todo 필드명 변경
+ */
 @Entity({ name: 'follow' })
 @ObjectType()
-export class FollowEntity {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => String)
-  id: string;
+export class FollowEntity extends BaseEntity {
+  @Column({
+    type: 'uuid',
+    name: 'user1Id',
+    comment: '팔로우하는 유저 ID',
+  })
+  @Field(() => String, {
+    description: '팔로우하는 유저 ID',
+    deprecationReason: '필드명이 변경 될 예정입니다.',
+  })
+  user1Id: string;
 
-  @Field(() => UserEntity)
-  @ManyToOne(() => UserEntity)
+  @Column({
+    type: 'uuid',
+    name: 'user2Id',
+    comment: '팔로잉하는 유저 ID',
+  })
+  @Field(() => String, {
+    description: '팔로잉하는 유저 ID',
+    deprecationReason: '필드명이 변경 될 예정입니다.',
+  })
+  user2Id: string;
+
+  @ManyToOne(() => UserEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @Field(() => UserEntity, {
+    description: '팔로우하는 user',
+    deprecationReason: '필드명이 변경 될 예정입니다.',
+  })
   user1: UserEntity;
 
-  @Field(() => UserEntity)
-  @ManyToOne(() => UserEntity)
+  @ManyToOne(() => UserEntity, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @Field(() => UserEntity, {
+    description: '팔로잉하는 user',
+    deprecationReason: '필드명이 변경 될 예정입니다.',
+  })
   user2: UserEntity;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
 }
